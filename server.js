a784e82798251a31a89c94cba4441a59a8c46ef1
Steve Romm <smr310@gmail.com>
2018-07-12 17:34:27 -0400

    test

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+const cors = require('cors');
+const { CLIENT_ORIGIN } = require('./config');
+
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+);
 
 app.get('/api/*', (req, res) => {
-    res.json({ ok: true });
+    res.json({ ok: true, weather: 'sunny' });
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
