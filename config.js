83ec3a97f0e696ba0f45dbd6e243034750eef429
Steve Romm <smr310@gmail.com>
2018-07-12 17:42:43 -0400

    test

---
+++ config.js
@@ -0,0 +1,5 @@
+'use strict';
+
+// exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://dbuser1:dbpw1@ds221609.mlab.com:21609/node_cap_db';
+// exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://dbuser1:dbpw1@ds133570.mlab.com:33570/node_cap_db_test';
+exports.PORT = process.env.PORT || 8080;
