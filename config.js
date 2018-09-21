5b270ef03f54d10026659f53cd808bed3a288c63
Steve Romm <smr310@gmail.com>
2018-09-21 07:48:45 -0400

    clean up

--- config.js
+++ config.js
@@ -9,4 +9,5 @@ exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
 
 exports.JWT_SECRET = process.env.JWT_SECRET || 'secretString'
 
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://dbuser1:dbpw12@ds111492.mlab.com:11492/travelbak_test'
+
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost:27017/travelBak_test'
